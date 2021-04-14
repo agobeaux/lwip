c4740ed6d1d464821c354246a9b7304ff6131ae4
agobeaux <alexandre.gobeaux@student.uclouvain.be>
2021-04-15 00:12:15 +0200

    Launch scripts: use build dir cmake and enable set -xe

--- launch-client.sh
+++ launch-client.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
+
+# stop on error
+set -e
+
+# show commands used
+set -x
+
+echo The client corresponds to tap1 and HW_ADDR_5=0x88 and IP addr 192.168.1.4
+
 # Create network interfaces
 sudo ./contrib/ports/unix/setup-tapif
 
-cmake . -DCLIENT_OR_SERVER=CLIENT && make
+# delete to be sure it will be made again
+rm -f ./build/contrib/ports/unix/example_app_client/example_app_client
+
+cd build
+cmake . -DCLIENT_OR_SERVER=CLIENT; cmake --build .
+cd ..
 
 # launch app
 # (maybe launch "cmake ." and "make" if changes occurred)
-sudo ./contrib/ports/unix/example_app_client/example_app_client
+sudo ./build/contrib/ports/unix/example_app_client/example_app_client
 
 # grep -r "hard-coded" to find where the bpf file was hard-coded.
 # ./contrib/ports/unix/example_app/CMakeLists.txt and ./src/Filelists.cmake -> useful
--- launch-server.sh
+++ launch-server.sh
@@ -1,12 +1,27 @@
 #!/bin/sh
+
+# stop on error
+set -e
+
+# show commands
+set -x
+
+echo The server corresponds to tap0 and HW_ADDR_5=0x01 and IP addr 192.168.1.3
+
 # Create network interfaces
 sudo ./contrib/ports/unix/setup-tapif
 
-cmake . -DCLIENT_OR_SERVER=SERVER && make
+# Delete executable to be sure it will be built again
+rm -f ./build/contrib/ports/unix/example_app/example_app
+
+cd build
+cmake . -DCLIENT_OR_SERVER=SERVER; cmake --build .
+cd ..
+
 
 # launch app
 # (maybe launch "cmake ." and "make" if changes occurred)
-sudo ./contrib/ports/unix/example_app/example_app
+sudo ./build/contrib/ports/unix/example_app/example_app
 
 # grep -r "hard-coded" to find where the bpf file was hard-coded.
 # ./contrib/ports/unix/example_app/CMakeLists.txt and ./src/Filelists.cmake -> useful
