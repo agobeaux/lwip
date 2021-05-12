agobeaux/lwip

commit afe257deece49e0e23d6023265c94491b0283316

Change structure: cnx in pcb, option parsers organized, option writers to enable
uto, rto: add functions that enable specific TCP option writer eBPF programs in server.

uto, rto: register parsers in client. Add a function to register tcp_option_parsers that belong to an option and (possibly) an experimentation ID (ExID).

Add ebpf_options_parser_bpf_code, Add ebpf_options_parser_bpf_code_253 and Add ebpf_options_parser_bpf_code_254: arrays of strings that contain the path to an eBPF program that allows to parse a TCP option with a certain option and ExID.

tcp.c: add user timeout behaviour to LWIP. Add timeout function along with the last_ack_received_tmr variable. This variable is a way to know the last time we received a segment acknowledging new data. This is an approximation to what we should have with the User TimeOut: the time for which a segment has not been ACKed.

Add cnx to pcb instead of pcb to cnx. More suitable for LWIP since the structure is already done with the tcp_pcb's. Also allows not to create a cnx every time we call an eBPF function.

Writing options: call now a single functions in ubpf.c that will call the writer functions. At the moment, only hard-coded calls. In the future, it should be interesting to have some kind of linked list. Another interesting thing would be to have a function that determines if the option_writer should write. Indeed, at the moment, we can not allow an option_writer to write only when it wants to write. For example, if a plugin would like to write an option only under some circumstances, it can not be able to do that here. It would thus be interesting to run those functions first, to be able to modify a global ebpf_options_length that would be used when trying to get the length of the options that have been written by eBPF programs.

contrib/examples/example_app/test.c
@@ -347,11 +347,6 @@ test_netif_init(void)
 #endif /* PPPOS_SUPPORT */
 #endif  /* USE_PPP */
 
-printf("heyhey, trying\n");
-/*run_ubpf();*/
-/* TODO: if I delete previous line, tcp_out.c does not compile, 
-         even if I let the include of library
-*/
 
 #if USE_ETHERNET
 #if LWIP_IPV4
@@ -360,6 +355,11 @@ printf("heyhey, trying\n");
   ip4_addr_set_zero(&netmask);
   ipaddr.addr = 0x0301A8C0UL; /* address 192.168.1.3 */
   netmask.addr = 0X00FFFFFFUL;
+
+  /* Plugins used */
+  set_use_uto_option();
+  set_use_rto_option();
+
 #if USE_ETHERNET_TCPIP
 #if USE_DHCP
   printf("Starting lwIP, local interface IP is dhcp-enabled\n");

contrib/examples/example_app_client/test.c
@@ -353,12 +353,6 @@ test_netif_init(void)
 #endif /* PPPOS_SUPPORT */
 #endif  /* USE_PPP */
 
-printf("heyhey, trying\n");
-/*run_ubpf();*/
-/* TODO: if I delete previous line, tcp_out.c does not compile,
-         even if I let the include of library
-*/
-
 #if USE_ETHERNET
 #if LWIP_IPV4
   ip4_addr_set_zero(&gw);
@@ -366,6 +360,19 @@ printf("heyhey, trying\n");
   ip4_addr_set_zero(&netmask);
   ipaddr.addr = 0x0401A8C0UL; /* address 192.168.1.4 0x0401A8C0UL*/
   netmask.addr = 0X00FFFFFFUL;
+
+  /* Plugins used */
+  ubpf_register_tcp_option_parser(
+    "/home/agobeaux/Desktop/M2Q1/MASTER_THESIS/VM_folder/lwip_programs/externals/lwip/ubpf/parse_tcp_option.bpf",
+    28,
+    0 /* useless param here */
+  );
+  ubpf_register_tcp_option_parser(
+    "/home/agobeaux/Desktop/M2Q1/MASTER_THESIS/VM_folder/lwip_programs/externals/lwip/ubpf/parse_tcp_rto_option.bpf",
+    253,
+    0x12EF
+  );
+
 #if USE_ETHERNET_TCPIP
 #if USE_DHCP
   printf("Starting lwIP, local interface IP is dhcp-enabled\n");

src/core/tcp_in.c
@@ -1220,7 +1220,7 @@ tcp_receive(struct tcp_pcb *pcb)
                 /* Inflate the congestion window */
                 TCP_WND_INC(pcb->cwnd, pcb->mss);
               }
-              if (pcb->dupacks >= 3 || ebpf_is_thin_stream(pcb)) { /* TODO: should thin_stream also trigger the congestion window change ? */
+              if (pcb->dupacks >= 3 ) { /*|| ebpf_is_thin_stream(pcb)) {*/ /* TODO: should thin_stream also trigger the congestion window change ? */
                 /* Do fast retransmit (checked via TF_INFR, not via dupacks count) */
                 tcp_rexmit_fast(pcb);
               }
@@ -1253,6 +1253,9 @@ tcp_receive(struct tcp_pcb *pcb)
       /* Reset the fast retransmit variables. */
       pcb->dupacks = 0;
       pcb->lastack = ackno;
+
+      /* Reset the unacknowledgement timer */
+      pcb->last_ack_received_tmr = tcp_ticks;
 
       /* Update the congestion control variables (cwnd and
          ssthresh). */

src/core/tcp_out.c
@@ -1392,6 +1392,7 @@ tcp_output(struct tcp_pcb *pcb)
       tcp_clear_flags(pcb, TF_ACK_DELAY | TF_ACK_NOW);
     }
     snd_nxt = lwip_ntohl(seg->tcphdr->seqno) + TCP_TCPLEN(seg);
+    bool has_unacked_data = (pcb->snd_nxt != pcb->lastack);
     if (TCP_SEQ_LT(pcb->snd_nxt, snd_nxt)) {
       pcb->snd_nxt = snd_nxt;
     }
@@ -1400,6 +1401,10 @@ tcp_output(struct tcp_pcb *pcb)
       seg->next = NULL;
       /* unacked list is empty? */
       if (pcb->unacked == NULL) {
+        if (!has_unacked_data && pcb->snd_nxt != pcb->lastack) {
+          /* Before this segment, every data sent was acked. Now, not anymore => reset last_ack_received_tmr */
+          pcb->last_ack_received_tmr = tcp_ticks;
+        }
         pcb->unacked = seg;
         useg = seg;
         /* unacked list is not empty? */
@@ -1549,7 +1554,7 @@ tcp_output_segment(struct tcp_seg *seg, struct tcp_pcb *pcb, struct netif *netif
     *(opts++) = PP_HTONL(0x01010402);
   }
 #endif
-  opts = ebpf_write_tcp_uto_option(pcb, opts);
+  opts = ebpf_write_tcp_options(pcb, opts);
 
   /* Set retransmission timer running if it is not currently enabled
      This must be set before checking the route. */
@@ -1926,7 +1931,7 @@ tcp_output_fill_options(struct tcp_pcb *pcb, struct pbuf *p, u8_t optflags, u8_t
 #ifdef LWIP_HOOK_TCP_OUT_ADD_TCPOPTS
   opts = LWIP_HOOK_TCP_OUT_ADD_TCPOPTS(p, tcphdr, pcb, opts);
 #endif
-  opts = ebpf_write_tcp_uto_option(pcb, opts);
+  opts = ebpf_write_tcp_options(pcb, opts);
 
   LWIP_UNUSED_ARG(pcb);
   LWIP_UNUSED_ARG(sacks_len);
@@ -2199,8 +2204,13 @@ tcp_zero_window_probe(struct tcp_pcb *pcb)
 
   /* The byte may be acknowledged without the window being opened. */
   snd_nxt = lwip_ntohl(seg->tcphdr->seqno) + 1;
+  bool has_unacked_data = (pcb->snd_nxt != pcb->lastack);
   if (TCP_SEQ_LT(pcb->snd_nxt, snd_nxt)) {
     pcb->snd_nxt = snd_nxt;
+  }
+  if (!has_unacked_data && pcb->snd_nxt != pcb->lastack) {
+    /* Before this segment, every data sent was acked. Now, not anymore => reset last_ack_received_tmr */
+    pcb->last_ack_received_tmr = tcp_ticks;
   }
   tcp_output_fill_options(pcb, p, 0, 0);
 

src/include/lwip/tcp.h
@@ -237,6 +237,19 @@ struct tcp_pcb_listen {
 #endif /* TCP_LISTEN_BACKLOG */
 };
 
+/* Context structure, needed for plugins */
+typedef struct {
+
+  /* Number of inputs for the plugin */
+  int inputc;
+
+  /* Actual inputs, all converted to uint64_t in order to allow for pointers */
+  uint64_t *inputv;
+
+  /* Fields required for the plugins to operate */
+  s16_t rto_max; /* max value of retransmission time-out allowed (in ticks of TCP_SLOW_INTERVAL) */
+
+} tcp_ubpf_cnx_t;
 
 /** the TCP protocol control block */
 struct tcp_pcb {
@@ -244,6 +257,9 @@ struct tcp_pcb {
   IP_PCB;
 /** protocol specific PCB members */
   TCP_PCB_COMMON(struct tcp_pcb);
+
+/** Context of the TCP connexion, important for plugins */
+  tcp_ubpf_cnx_t cnx;
 
   /* ports are in host byte order */
   u16_t remote_port;
@@ -278,6 +294,9 @@ struct tcp_pcb {
   u8_t polltmr, pollinterval;
   u8_t last_timer;
   u32_t tmr;
+  u32_t last_ack_received_tmr; /* time when the last packet acknowledging new data was received */
+
+  u64_t user_timeout; /* time after which the connexion is considered as timed out (in milliseconds) */
 
   /* receiver variables */
   u32_t rcv_nxt;   /* next seqno expected */
@@ -303,7 +322,6 @@ struct tcp_pcb {
   s16_t sa, sv; /* @see "Congestion Avoidance and Control" by Van Jacobson and Karels */
 
   s16_t rto;      /* retransmission time-out (in ticks of TCP_SLOW_INTERVAL) */
-  s16_t rto_max;  /* max value of retransmission time-out allowed (in ticks of TCP_SLOW_INTERVAL) */
   u8_t nrtx;      /* number of retransmissions */
 
   /* fast retransmit/recovery */
@@ -389,20 +407,6 @@ struct tcp_pcb {
   u8_t rcv_scale;
 #endif
 };
-
-/* Context structure, needed for plugins */
-typedef struct {
-
-  /* TCP PCB on which the plugin will operate */
-  struct tcp_pcb *pcb;
-
-  /* Number of inputs for the plugin */
-  int inputc;
-
-  /* Actual inputs, all converted to uint64_t in order to allow for pointers */
-  uint64_t *inputv;
-
-} tcp_ubpf_cnx_t;
 
 #if LWIP_EVENT_API
 

ubpf/getset.c
@@ -2,10 +2,10 @@
 
 
 /*
- * Returns the pcb corresponding to the tcp_ubpf context
+ * Returns the tcp_ubpf context corresponding to the tcp pcb
  */
-struct tcp_pcb *get_pcb(tcp_ubpf_cnx_t *cnx) {
-    return cnx->pcb;
+tcp_ubpf_cnx_t *get_cnx(struct tcp_pcb *pcb) {
+    return &(pcb->cnx);
 }
 
 /*
@@ -121,15 +121,15 @@ s16_t get_rto(struct tcp_pcb *pcb) {
     return pcb->rto;
 }
 
-s16_t get_rto_max(struct tcp_pcb *pcb) {
+s16_t get_rto_max(tcp_ubpf_cnx_t *cnx) {
     /* TODO: modify using TCP_TMR_INTERVAL */
-    printf("Returning rto_max: %d\n", pcb->rto_max);
-    return pcb->rto_max;
+    printf("Returning rto_max: %d\n", cnx->rto_max);
+    return cnx->rto_max;
 }
 
-void set_rto_max(struct tcp_pcb *pcb, u16_t timeout) {
+void set_rto_max(tcp_ubpf_cnx_t *cnx, u16_t timeout) {
     /* TODO: modify using TCP_TMR_INTERVAL */
-    pcb->rto_max = timeout;
+    cnx->rto_max = timeout;
     printf("rto_max set to %u\n", timeout);
     printf("rto_max set to 0x%x\n", timeout);
 }

ubpf/getset.h
@@ -5,7 +5,7 @@
 /*
  * Returns the pcb corresponding to the tcp_ubpf context
  */
-struct tcp_pcb *get_pcb(tcp_ubpf_cnx_t *cnx);
+tcp_ubpf_cnx_t *get_cnx(struct tcp_pcb *pcb);
 /*
  * Returns the (index+1)th input passed to the tcp_ubpf context or NULL if it is out of range
  */
@@ -51,8 +51,8 @@ void set_opt(u32_t *opts, int index, u32_t value);
 
 s16_t get_rto(struct tcp_pcb *pcb);
 
-s16_t get_rto_max(struct tcp_pcb *pcb);
+s16_t get_rto_max(tcp_ubpf_cnx_t *cnx);
 
-void set_rto_max(struct tcp_pcb *pcb, u16_t timeout);
+void set_rto_max(tcp_ubpf_cnx_t *cnx, u16_t timeout);
 
 void membound_fail(uint64_t val, uint64_t mem_ptr, uint64_t stack_ptr);

commit 5d1366a49662142c9ed52dcc9d5e7af4260d3810

Fix tcp_accept crash, free allocated VM memory, change parsing, update UTO, change N_ARGS based on compiler
Commit afe257deece49e0e23d6023265c94491b0283316 introduced bugs as it changed the tcp_pcb structure. The bug occurred when the server kept running against a client (which required the server to call an eBPF program with an input). The bug consisted in pcb->listener->accept being replaced by 1 (the value of cnx.inputc). The reason was a collision between pcb->cnx and pcb->listener. In LWIP, to set the accept function, the pcb is cast in a tcp_pcb_listener and the accept function is thus set. When setting (pcb->cnx).inputc, it thus replaced the value of the accept function. The cnx field has been put at the end of the tcp_pcb structure.

When calling eBPF, the memory allocated for the VM was not freed, this is now the case. Also, when the VM crashes, he return value is now set to (uint64_t)-1.

N_ARGS macro: the macro now depends on the compiler according to __GNUC__ and __STDC_VERSION__.

Parsing eBPF programs: the length of the TCP options is read before calling ebpf functions. length is now an arg of the eBPF program called. Reason: easier for updating tcp_optidx, gives more power to the implementation to know what is going on. Parsing both the kind (opt) and the value (length) is not a problem, we know those will be next to each other in every TCP option.
Return value is now either ERR_OK if everything went fine, ERR_VAL if no parser was foun or if the format received was in an unexpected format, ERR_ARG if the eBPF program crashed or if the option length had a bad value (<2). The last error code causes the program to stop.

UTO option: now the UTO option parser sets user_timeout instead of rto_max. Functions get_user_timeout and set_user_timeout avec been added to the API called by eBPF (getset.c)

contrib/examples/example_app/lwipcfg.h
@@ -1,0 +1,80 @@
+/* COPY PASTE OF EXAMPLE TO MAKE IT WORK */
+
+/**
+ * Additional settings for the win32 port.
+ * Copy this to lwipcfg_msvc.h and make the config changes you need.
+ */
+
+/* configuration for this port */
+#define PPP_USERNAME  "Admin"
+#define PPP_PASSWORD  "pass"
+
+/** Define this to the index of the windows network adapter to use */
+#define PACKET_LIB_ADAPTER_NR         1
+/** Define this to the GUID of the windows network adapter to use
+ * or NOT define this if you want PACKET_LIB_ADAPTER_NR to be used */
+/*#define PACKET_LIB_ADAPTER_GUID       "00000000-0000-0000-0000-000000000000"*/
+/*#define PACKET_LIB_GET_ADAPTER_NETADDRESS(addr) IP4_ADDR((addr), 192,168,1,0)*/
+/*#define PACKET_LIB_QUIET*/
+
+/* If these 2 are not defined, the corresponding config setting is used */
+/* #define USE_DHCP    0 */
+/* #define USE_AUTOIP  0 */
+
+/* #define USE_PCAPIF 1 */
+#define LWIP_PORT_INIT_IPADDR(addr)   IP4_ADDR((addr), 192,168,1,200)
+#define LWIP_PORT_INIT_GW(addr)       IP4_ADDR((addr), 192,168,1,1)
+#define LWIP_PORT_INIT_NETMASK(addr)  IP4_ADDR((addr), 255,255,255,0)
+
+/* remember to change this MAC address to suit your needs!
+   the last octet will be increased by netif->num for each netif */
+#define LWIP_MAC_ADDR_BASE            {0x00,0x01,0x02,0x03,0x04,0x05}
+
+/* #define USE_SLIPIF 0 */
+/* #define SIO_USE_COMPORT 0 */
+#ifdef USE_SLIPIF
+#if USE_SLIPIF
+#define LWIP_PORT_INIT_SLIP1_IPADDR(addr)   IP4_ADDR((addr), 192, 168,   2, 2)
+#define LWIP_PORT_INIT_SLIP1_GW(addr)       IP4_ADDR((addr), 192, 168,   2, 1)
+#define LWIP_PORT_INIT_SLIP1_NETMASK(addr)  IP4_ADDR((addr), 255, 255, 255, 0)
+#if USE_SLIPIF > 1
+#define LWIP_PORT_INIT_SLIP2_IPADDR(addr)   IP4_ADDR((addr), 192, 168,   2, 1)
+#define LWIP_PORT_INIT_SLIP2_GW(addr)       IP4_ADDR((addr), 0,     0,   0, 0)
+#define LWIP_PORT_INIT_SLIP2_NETMASK(addr)  IP4_ADDR((addr), 255, 255, 255, 0)*/
+#endif /* USE_SLIPIF > 1 */
+#endif /* USE_SLIPIF */
+#endif /* USE_SLIPIF */
+
+/* configuration for applications */
+
+#define LWIP_CHARGEN_APP              0
+#define LWIP_DNS_APP                  0
+#define LWIP_HTTPD_APP                0
+/* Set this to 1 to use the netconn http server,
+ * otherwise the raw api server will be used. */
+/*#define LWIP_HTTPD_APP_NETCONN     */
+#define LWIP_NETBIOS_APP              0
+#define LWIP_NETIO_APP                0
+#define LWIP_MDNS_APP                 0
+#define LWIP_MQTT_APP                 0
+#define LWIP_PING_APP                 0
+#define LWIP_RTP_APP                  0
+#define LWIP_SHELL_APP                0
+#define LWIP_SNMP_APP                 0
+#define LWIP_SNTP_APP                 0
+#define LWIP_SOCKET_EXAMPLES_APP      0
+#define LWIP_TCPECHO_APP              0
+/* Set this to 1 to use the netconn tcpecho server,
+ * otherwise the raw api server will be used. */
+#define LWIP_TCPECHO_APP_NETCONN
+#define LWIP_TFTP_APP                 0
+#define LWIP_TFTP_CLIENT_APP          0
+#define LWIP_UDPECHO_APP              0
+#define LWIP_LWIPERF_APP              1
+#define LWIP_LWIPERF_CLIENT_APP       0
+
+/*#define USE_DHCP    1*/
+/*#define USE_AUTOIP  1*/
+
+/* define this to your custom application-init function */
+/* #define LWIP_APP_INIT my_app_init() */

contrib/examples/example_app/test.c
@@ -360,6 +360,17 @@ test_netif_init(void)
   set_use_uto_option();
   set_use_rto_option();
 
+  ubpf_register_tcp_option_parser(
+    "/home/agobeaux/Desktop/M2Q1/MASTER_THESIS/VM_folder/lwip_programs/externals/lwip/ubpf/parse_tcp_uto_option.bpf",
+    28,
+    0 /* useless param here */
+  );
+  ubpf_register_tcp_option_parser(
+    "/home/agobeaux/Desktop/M2Q1/MASTER_THESIS/VM_folder/lwip_programs/externals/lwip/ubpf/parse_tcp_rto_option.bpf",
+    253,
+    0x12EF
+  );
+
 #if USE_ETHERNET_TCPIP
 #if USE_DHCP
   printf("Starting lwIP, local interface IP is dhcp-enabled\n");

contrib/examples/example_app_client/test.c
@@ -363,7 +363,7 @@ test_netif_init(void)
 
   /* Plugins used */
   ubpf_register_tcp_option_parser(
-    "/home/agobeaux/Desktop/M2Q1/MASTER_THESIS/VM_folder/lwip_programs/externals/lwip/ubpf/parse_tcp_option.bpf",
+    "/home/agobeaux/Desktop/M2Q1/MASTER_THESIS/VM_folder/lwip_programs/externals/lwip/ubpf/parse_tcp_uto_option.bpf",
     28,
     0 /* useless param here */
   );

src/core/tcp_in.c
@@ -2009,20 +2009,10 @@ tcp_parseopt(struct tcp_pcb *pcb)
           if (err == ERR_OK) {
             LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: other, parsed by eBPF\n"));
           } else if (err == ERR_VAL) {
-            LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: other, parsing by eBPF crashed\n"));
-            return;
+            LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: other, either no eBPF parser was found for this option or the format received was unexpected\n"));
           } else { /* ERR_ARG, if opt non recognized */
-            LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: other, couldn't be parsed by eBPF\n"));
-            data = tcp_get_next_optbyte();
-            if (data < 2) {
-              LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: bad length\n"));
-              /* If the length field is zero, the options are malformed
-                and we don't process them further. */
-              return;
-            }
-            /* All other options have a length field, so that we easily
-              can skip past them. */
-            tcp_optidx += data - 2;
+            LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: other, either the parsing by eBPF crashed or bad option length\n"));
+            return;
           }
           /* LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: other\n")); */
 

src/core/tcp_in_helper.c
@@ -24,7 +24,7 @@ tcp_get_next_optbyte(void)
     u8_t *opts = (u8_t *)tcphdr + TCP_HLEN;
     printf("after setting opts\n");
     u8_t ret = opts[optidx];
-    printf("after SETTING ret !!! not problem should happen\n"); fflush(stdout);
+    printf("after SETTING ret !!! no problem should happen\n"); fflush(stdout);
     return ret;
     //return opts[optidx];
   } else {

src/core/tcp_out.c
@@ -79,8 +79,7 @@
 #endif
 
 /* UBPF INCLUDE */
-#include "vm/ubpf.h" /* TODO: change, include in CMake file in directories -> easy path */
-/* not needed as it is linked? Still needed, u know why */
+#include "vm/ubpf.h"
 
 #include <string.h>
 

src/include/lwip/tcp.h
@@ -258,9 +258,6 @@ struct tcp_pcb {
 /** protocol specific PCB members */
   TCP_PCB_COMMON(struct tcp_pcb);
 
-/** Context of the TCP connexion, important for plugins */
-  tcp_ubpf_cnx_t cnx;
-
   /* ports are in host byte order */
   u16_t remote_port;
 
@@ -406,6 +403,10 @@ struct tcp_pcb {
   u8_t snd_scale;
   u8_t rcv_scale;
 #endif
+
+/** Context of the TCP connexion, important for plugins */
+  tcp_ubpf_cnx_t cnx;
+
 };
 
 #if LWIP_EVENT_API

ubpf/getset.c
@@ -134,6 +134,19 @@ void set_rto_max(tcp_ubpf_cnx_t *cnx, u16_t timeout) {
     printf("rto_max set to 0x%x\n", timeout);
 }
 
+u64_t get_user_timeout(struct tcp_pcb *pcb) {
+    /* TODO: modify using TCP_TMR_INTERVAL */
+    printf("Returning user_timeout: %u\n", pcb->user_timeout);
+    return pcb->user_timeout;
+}
+
+void set_user_timeout(struct tcp_pcb *pcb, u64_t timeout) {
+    /* TODO: modify using TCP_TMR_INTERVAL */
+    pcb->user_timeout = timeout;
+    printf("user_timeout set to %u\n", timeout);
+    printf("user_timeout set to 0x%x\n", timeout);
+}
+
 void membound_fail(uint64_t val, uint64_t mem_ptr, uint64_t stack_ptr) {
     printf("Out of bound access with val 0x%lx, start of mem is 0x%lx, top of stack is 0x%lx\n", val, mem_ptr, stack_ptr);
 }

ubpf/getset.h
@@ -55,4 +55,8 @@ s16_t get_rto_max(tcp_ubpf_cnx_t *cnx);
 
 void set_rto_max(tcp_ubpf_cnx_t *cnx, u16_t timeout);
 
+u64_t get_user_timeout(struct tcp_pcb *pcb);
+
+void set_user_timeout(struct tcp_pcb *pcb, u64_t timeout);
+
 void membound_fail(uint64_t val, uint64_t mem_ptr, uint64_t stack_ptr);

ubpf/parse_tcp_option.c
@@ -1,39 +1,0 @@
-#include <stdio.h>
-#include "lwip/tcp.h"
-#include "lwip/err.h"
-#include "getset.h"
-
-#define TCP_OPT_UTO 28 /* User TimeOut option kind: 28 */
-int parse_tcp_uto_option(struct tcp_pcb *pcb) {
-	/* In this function, we parse the different TCP options that we can */
-	tcp_ubpf_cnx_t *cnx = get_cnx(pcb);
-	u8_t granularity;
-	u16_t timeout;
-	//help_printf_str("opt in ubpf is: "); help_printf_uint8_t(opt);
-	/* TODO: change it back to have parse_tcp_option or one plugin by option? (better I think) */
-	//switch (opt) {
-		//case TCP_OPT_UTO:
-			if (tcp_get_next_optbyte() != 4) {
-				/* Length is different than 4, not normal */
-				help_printf_str("ERROR: tcp_get_next_optbyte should be 4 for UTO");
-				return ERR_VAL;
-			}
-			timeout = tcp_get_next_optbyte();
-    	timeout |= (tcp_get_next_optbyte() << 8);
-			timeout = custom_ntohs(timeout);
-			granularity = (timeout & 0x8000) >> 15;
-			timeout &= 0x7fff; // filter out the granularity part
-			help_printf_str("granularity received :");
-			help_printf_uint8_t(granularity);
-			set_rto_max(cnx, timeout);
-			//pcb->rto_max = timeout; /* TODO: should be replaced by a setter to allow flexibility */
-			/* TODO: what to do about granularity ?*/
-			// TODO: update uto in pcb?
-			// TODO: tcp_get_next_optbyte() should be available from here (file tcp_in.c, not cool)
-			//break;
-		//default:
-			//help_printf_str("in default, opt is: "); help_printf_uint8_t(opt);
-			//return ERR_ARG; /* Could not parse the option */
-	//}
-	return ERR_OK; /* Parsed option successfully */
-}

ubpf/parse_tcp_uto_option.c
@@ -1,0 +1,28 @@
+#include <stdio.h>
+#include "lwip/tcp.h"
+#include "lwip/err.h"
+#include "getset.h"
+
+#define TCP_OPT_UTO 28 /* User TimeOut option kind: 28 */
+int parse_tcp_uto_option(struct tcp_pcb *pcb) {
+	/* In this function, we parse the different TCP options that we can */
+	tcp_ubpf_cnx_t *cnx = get_cnx(pcb);
+	u8_t option_length = get_input(cnx, 0);
+	if (option_length != 4) {
+		/* Length is different than 4, not normal */
+		help_printf_str("ERROR: tcp_get_next_optbyte should be 4 for UTO");
+		return ERR_VAL;
+	}
+
+	u8_t granularity;
+	u16_t timeout;
+	timeout = tcp_get_next_optbyte();
+	timeout |= (tcp_get_next_optbyte() << 8);
+	timeout = custom_ntohs(timeout);
+	granularity = (timeout & 0x8000) >> 15;
+	timeout &= 0x7fff; // filter out the granularity part
+	help_printf_str("granularity received :");
+	help_printf_uint8_t(granularity);
+	set_user_timeout(pcb, timeout);
+	return ERR_OK; /* Parsed option successfully */
+}

ubpf/plugins/delayed_ack/is_ack_needed.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "lwip/tcp.h"
-#include "getset.h"
 
 int is_ack_needed(struct tcp_pcb *pcb) {
 	char *str = "Currently launching eBPF function is_ack_needed!\n";

ubpf/vm/ubpf.c
@@ -76,29 +76,66 @@ int ebpf_should_drop_connection_rto(struct tcp_pcb *pcb) { /* u64_t time_waiting
 
 int ebpf_parse_tcp_option(struct tcp_pcb *pcb, u8_t opt) {
     printf("ebpf_parse_tcp_option\n");
-    /* TODO: temporary options: those should be taken care of separately */
-
     const char *code_filename;
+    printf("Before returning ERR_ARG in ebpf_parse_tcp_option\n");
+    return ERR_ARG;
+    printf("Checking opt\n");
+    u16_t previous_tcp_optidx = tcp_optidx;
+    u8_t option_length = tcp_get_next_optbyte();
+
+    if (option_length < 2) {
+        LWIP_DEBUGF(TCP_INPUT_DEBUG, ("tcp_parseopt: bad length\n"));
+        /* If the length field is zero, the options are malformed
+        and we don't process them further. */
+        return ERR_ARG;
+    }
 
     if (opt == 253 || opt == 254) {
-        u8_t option_length = tcp_get_next_optbyte();
         u16_t exID = custom_ntohs(tcp_get_next_optbyte() | (tcp_get_next_optbyte() << 8));
         if (opt == 253) {
             code_filename = ebpf_options_parser_bpf_code_253[exID];
         } else { /* opt == 254 */
             code_filename = ebpf_options_parser_bpf_code_254[exID];
         }
-        if (code_filename) return run_ubpf_with_args(pcb, code_filename, option_length);
+        if (code_filename) {
+            printf("code_filename exp: %s\n", code_filename);
+            uint64_t ret = run_ubpf_with_args(pcb, code_filename, option_length);
+            if (ret == (uint64_t)-1) {
+                /* VM crashed here */
+                return ERR_ARG;
+            }
+            tcp_optidx = previous_tcp_optidx + option_length;
+            return ERR_OK;
+        }
         else {
             printf("No parser corresponds to opt %u and ExID 0x%x\n", opt, exID);
-            return ERR_ARG; /* Unknown TCP option is being used */
+            /* Skip the option */
+            tcp_optidx = previous_tcp_optidx + option_length;
+            /* All other options have a length field, so that we easily
+              can skip past them. */
+
+            return ERR_VAL; /* Unknown TCP option is being used */
         }
     } else {
         code_filename = ebpf_options_parser_bpf_code[opt];
-        if (code_filename) return run_ubpf_with_args(pcb, code_filename);
+        if (code_filename) {
+            printf("code_filename non-exp: %s\n", code_filename);
+            uint64_t ret = run_ubpf_with_args(pcb, code_filename, option_length);// ,NULL);
+            if (ret == (uint64_t)-1) {
+                /* VM crashed here */
+                return ERR_ARG;
+            }
+            tcp_optidx = previous_tcp_optidx + option_length;
+            return ERR_OK;
+        }
         else {
             printf("No parser corresponds to opt %u\n", opt);
-            return ERR_ARG; /* Unknown TCP option is being used */
+            /* Skip the option */
+            tcp_optidx = previous_tcp_optidx + option_length;
+            /* All other options have a length field, so that we easily
+              can skip past them. */
+
+            return ERR_VAL; /* Unknown TCP option is being used */
         }
     }
 
@@ -204,20 +241,26 @@ uint64_t run_ubpf_args(struct tcp_pcb *pcb, const char *code_filename, int n_arg
 
     code = readfile(code_filename, 1024*1024, &code_len);
     if (code == NULL) {
-        return 1;
+        if (mem) {
+            free(mem);
+        }
+        return (uint64_t)-1;
     }
 
     if (mem_filename != NULL) {
         mem = readfile(mem_filename, 1024*1024, &mem_len);
         if (mem == NULL) {
-            return 1;
+            return (uint64_t)-1;
         }
     }
 
     vm = ubpf_create();
     if (!vm) {
+        if (mem) {
+            free(mem);
+        }
         fprintf(stderr, "Failed to create VM\n");
-        return 1;
+        return (uint64_t)-1;
     }
 
     register_functions(vm);
@@ -244,8 +287,11 @@ uint64_t run_ubpf_args(struct tcp_pcb *pcb, const char *code_filename, int n_arg
     if (rv < 0) {
         fprintf(stderr, "Failed to load code: %s\n", errmsg);
         free(errmsg);
+        if (mem) {
+            free(mem);
+        }
         ubpf_destroy(vm);
-        return 1;
+        return (uint64_t)-1;
     }
 
     printf("Before JIT\n"); fflush(NULL); /* TODO: erase */
@@ -254,9 +300,12 @@ uint64_t run_ubpf_args(struct tcp_pcb *pcb, const char *code_filename, int n_arg
         printf("jit is true\n"); fflush(NULL); /* TODO: erase */
         fn = ubpf_compile(vm, &errmsg);
         if (fn == NULL) {
+            if (mem) {
+                free(mem);
+            }
             fprintf(stderr, "Failed to compile: %s\n", errmsg);
             free(errmsg);
-            return 1;
+            return (uint64_t)-1;
         }
         ret = fn(mem, mem_len);
     } else {
@@ -265,7 +314,9 @@ uint64_t run_ubpf_args(struct tcp_pcb *pcb, const char *code_filename, int n_arg
     }
 
     printf("0x%"PRIx64"\n", ret);
-
+    if (mem) {
+        free(mem);
+    }
     ubpf_destroy(vm);
 
     return ret; /* TODO: change, should not return int but something else, protooop_arg_t like PQUIC */
@@ -383,6 +434,8 @@ register_functions(struct ubpf_vm *vm)
     ubpf_register(vm, function_index++, "tcp_get_next_optbyte", tcp_get_next_optbyte);
     ubpf_register(vm, function_index++, "get_rto_max", get_rto_max);
     ubpf_register(vm, function_index++, "set_rto_max", set_rto_max);
+    ubpf_register(vm, function_index++, "get_user_timeout", get_user_timeout);
+    ubpf_register(vm, function_index++, "set_user_timeout", set_user_timeout);
     ubpf_register(vm, function_index++, "get_rto", get_rto);
     ubpf_register(vm, function_index++, "help_printf_sint16_t", help_printf_sint16_t);
     ubpf_register(vm, function_index++, "get_cnx", get_cnx);

ubpf/vm/ubpf.h
@@ -158,6 +158,7 @@ int ebpf_should_drop_connection_rto(struct tcp_pcb *pcb); /* u64_t time_waiting_
 
 /*
  * Parses tcp options
+ * Updates tcp_optidx and returns either ERR_OK if everything went fine, ERR_VAL if 
  */
 int ebpf_parse_tcp_option(struct tcp_pcb *pcb, u8_t opt);
 